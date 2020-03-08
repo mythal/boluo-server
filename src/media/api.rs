54fe993848cf2c57145b5c89688708669bcc5df0
Quan Brew <t@ioover.net>
2020-03-08 19:01:45 +0800

    fix mime type

--- src/media/api.rs
+++ src/media/api.rs
@@ -2,12 +2,14 @@ use serde::Deserialize;
 use uuid::Uuid;
 
 #[derive(Deserialize)]
+#[serde(rename_all = "camelCase")]
 pub struct Upload {
     pub filename: String,
     pub mime_type: Option<String>,
 }
 
 #[derive(Deserialize)]
+#[serde(rename_all = "camelCase")]
 pub struct MediaQuery {
     pub filename: Option<String>,
     pub id: Option<Uuid>,
