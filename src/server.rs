d689423c7f8fbe41a0df142d80baf66cf173b2c5
coppa <sortal@protonmail.com>
2020-08-24 17:40:48 +0800

    remove docker-compose and don't panic when server failed to load .env

--- src/server.rs
+++ src/server.rs
@@ -101,7 +101,7 @@ async fn handler(req: Request<Body>) -> Result<Response, hyper::Error> {
 
 #[tokio::main]
 async fn main() {
-    dotenv::dotenv().unwrap();
+    dotenv::dotenv().ok();
     let port: u16 = env::var("PORT").unwrap().parse().unwrap();
     logger::setup_logger(debug()).unwrap();
 
