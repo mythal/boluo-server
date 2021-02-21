59c23442e0244c1edb893847372b7c3b4f1359b3
coppa <sortal@protonmail.com>
2021-02-21 20:26:42 +0900

    logger for systemd

--- src/context.rs
+++ src/context.rs
@@ -17,3 +17,7 @@ pub fn debug() -> bool {
 pub fn secret() -> &'static str {
     &*SECRET.get_or_init(|| env::var("SECRET").unwrap())
 }
+
+pub fn is_systemd() -> bool {
+    *DEBUG.get_or_init(|| env::var("SYSTEMD").map(env_bool).unwrap_or(false))
+}
--- src/logger.rs
+++ src/logger.rs
@@ -1,26 +1,46 @@
 use fern::colors::{Color, ColoredLevelConfig};
-use log::LevelFilter;
+use log::{Level, LevelFilter};
+use crate::context::is_systemd;
+
+pub fn to_systemd_log_level(level: Level) -> u8 {
+    match level {
+        Level::Trace => 7,
+        Level::Debug => 6,
+        Level::Info => 5,
+        Level::Warn => 4,
+        Level::Error => 3,
+    }
+}
 
 pub fn setup_logger(debug: bool) -> Result<(), fern::InitError> {
     let level = if debug { LevelFilter::Debug } else { LevelFilter::Info };
     let color_config = ColoredLevelConfig::new()
-        .info(Color::BrightGreen)
-        .error(Color::BrightRed)
-        .warn(Color::Yellow)
+        .info(Color::Green)
+        .error(Color::BrightBlue)
+        .warn(Color::BrightYellow)
         .debug(Color::Magenta)
         .trace(Color::BrightCyan);
 
     fern::Dispatch::new()
         .format(move |out, message, record| {
-            out.finish(format_args!(
-                "[{:>5}]{}[{}] {}",
-                color_config.color(record.level()),
-                chrono::Local::now().format("[%Y-%m-%d][%H:%M:%S]"),
-                record.target(),
-                message
-            ))
+            if is_systemd() {
+                out.finish(format_args!(
+                    "<{}>[{}] {}",
+                    to_systemd_log_level(record.level()),
+                    record.target(),
+                    message
+                ))
+            } else {
+                out.finish(format_args!(
+                    "[{:>5}]{}[{}] {}",
+                    color_config.color(record.level()),
+                    chrono::Local::now().format("[%Y-%m-%d][%H:%M:%S]"),
+                    record.target(),
+                    message
+                ))
+            }
         })
-        .level(LevelFilter::Info)
+        .level(LevelFilter::Info) // minimum level needed to output
         .level_for("server", level)
         .chain(std::io::stdout())
         .apply()?;
