e213b0b230f16a559c4a80192cfaea76ebbd880a
coppa <sortal@protonmail.com>
2021-03-12 15:57:40 +0900

    disable CSRF

--- src/csrf.rs
+++ src/csrf.rs
@@ -10,51 +10,6 @@ use uuid::Uuid;
 pub async fn authenticate(req: &Request<Body>) -> Result<Session, AppError> {
     use hyper::Method;
     let session = session::authenticate(req).await?;
-    let method = req.method();
-    if method == Method::GET || method == Method::HEAD || req.headers().contains_key(AUTHORIZATION) {
-        return Ok(session);
-    }
-    let token = req
-        .headers()
-        .get(HeaderName::from_static("csrf-token"))
-        .and_then(|header_value| header_value.to_str().ok())
-        .ok_or_else(|| BadRequest("Not found CSRF token in the headers.".to_string()))?;
-
-    let (body, signature) = token
-        .rfind('.')
-        .map(|pos| token.split_at(pos))
-        .ok_or_else(|| BadRequest("Malformed token.".to_string()))?;
-    // signature: .[...]
-    let signature = &signature[1..];
-
-    let mut parts = body.split('.');
-
-    let session_id = parts
-        .next()
-        .and_then(|s| base64::decode(s).ok()) // decode.
-        .and_then(|bytes: Vec<u8>| Uuid::from_slice(&*bytes).ok()) // convert bytes to UUID.
-        .ok_or_else(|| BadRequest("Failed to parse CSRF token.".to_string()))?;
-
-    verify(body, &signature).ok_or_else(|| {
-        log::warn!("Session {}: Failed to verify the signature of CSRF token", session_id);
-        Unauthenticated
-    })?;
-
-    if session_id != session.id {
-        log::warn!("Session {}: CSRF and session is not matching.", session_id);
-        return Err(Unauthenticated);
-    }
-
-    let timestamp: u64 = parts
-        .next()
-        .and_then(|s| s.parse().ok())
-        .ok_or_else(|| BadRequest("Failed to parse timestamp".to_string()))?;
-
-    let now = now_unix_duration().as_secs();
-    if timestamp < now {
-        log::info!("Session {}: The CSRF token has timeout", session_id);
-        return Err(Unauthenticated);
-    }
     Ok(session)
 }
 
