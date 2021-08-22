b5649a6a9501f05fd9c077c3ac12f77d33d82e5a
Tachibana Kiyomi <me@yuru.me>
2021-08-22 23:17:38 +0900

    fix test

--- src/messages/sql/create.sql
+++ src/messages/sql/create.sql
@@ -9,8 +9,12 @@ INSERT INTO messages (
     is_action,
     is_master,
     whisper_to_users,
-    media_id
-) VALUES (
+    media_id,
+    order_date,
+    created,
+    modified
+)
+VALUES (
     COALESCE($1, uuid_generate_v1mc()),
     $2,
     $3,
@@ -21,6 +25,9 @@ INSERT INTO messages (
     $8,
     $9,
     $10,
-    $11
+    $11,
+    COALESCE(to_timestamp($12 / 1000.0) at time zone 'utc', now() at time zone 'utc'),
+    COALESCE(to_timestamp($12 / 1000.0) at time zone 'utc', now() at time zone 'utc'),
+    COALESCE(to_timestamp($12 / 1000.0) at time zone 'utc', now() at time zone 'utc')
 )
 RETURNING messages;
