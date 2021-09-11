3ce30adafac01f8a596baa9e7fb98d484394174a
Tachibana Kiyomi <me@yuru.me>
2021-09-11 10:13:25 +0900

    improve test script

--- test.sh
+++ test.sh
@@ -1,10 +1,8 @@
 #!/bin/bash
+set -e
 dropdb boluo_test
 createdb boluo_test
 psql boluo_test < schema.sql
 export TEST_DATABASE_URL="postgresql://${USER}@localhost/boluo_test"
-if [ ! -f .env ]
-then
-  export "$(xargs < .env)"
-fi
+export "$(xargs < .env)"
 cargo test
